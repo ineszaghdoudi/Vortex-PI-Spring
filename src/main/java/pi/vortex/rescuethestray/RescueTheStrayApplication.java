fc5185cceefea3efa83d07dcdf2d30e6d4e5e748
ineszaghdoudi <ineszaghdoudi99@gmail.com>
2023-04-10 10:50:18 +0100

    Creation du projet

---
+++ src/main/java/pi/vortex/rescuethestray/RescueTheStrayApplication.java
@@ -0,0 +1,13 @@
+package pi.vortex.rescuethestray;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RescueTheStrayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RescueTheStrayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pi/vortex/rescuethestray/RescueTheStrayApplicationTests.java
@@ -0,0 +1,13 @@
+package pi.vortex.rescuethestray;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RescueTheStrayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
