3ac6eb820f412a7a9197fdbf01878639ae2b3460
ineszaghdoudi <ineszaghdoudi99@gmail.com>
2023-04-14 16:37:04 +0100

    Crud Learning Resources Finished

--- src/main/java/pi/vortex/rescuethestray/controllers/LearningResourceController.java
+++ src/main/java/pi/vortex/rescuethestray/controllers/LearningResourceController.java
@@ -1,16 +1,12 @@
 package pi.vortex.rescuethestray.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pi.vortex.rescuethestray.entities.LearningResource;
 import pi.vortex.rescuethestray.interfaces.ILearningResourceService;
-import pi.vortex.rescuethestray.repositories.ILearningResourceRepo;
-import pi.vortex.rescuethestray.services.LearningResourceService;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class LearningResourceController {
@@ -25,9 +21,25 @@ public class LearningResourceController {
         return listLearningResources;
     }
 
+    @GetMapping("/retrieve-learning-resource/{learning_resource-id}")
+    public Optional<LearningResource> getLearningResourceById(@PathVariable("learning_resource-id") Long id_learningr) {
+        return iLearningResourceService.retrieveLearningResource(id_learningr);
+    }
+
     @PostMapping("/add-learning-resource")
     public LearningResource addLearningResource(@RequestBody LearningResource lr) {
         LearningResource learningResource=iLearningResourceService.addLearningResource(lr);
         return learningResource;
+    }
+
+    @PutMapping("/update-learning-resource")
+    public LearningResource updateLearningResource(@RequestBody LearningResource lr) {
+        LearningResource learningResource= iLearningResourceService.updateLearningResource(lr);
+        return learningResource;
+    }
+
+    @DeleteMapping("/remove-learning-resource/{learning_resource-id}")
+    public void removeLearningResource(@PathVariable("learning_resource-id") Long id_learningr) {
+        iLearningResourceService.removeLearningResource(id_learningr);
     }
 }
